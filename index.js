fc17e3fb6cfcd036e1cd3a40728f74e66f73a271
diarm0d <diarmuid.odonovan@protonmail.com>
2021-02-05 13:54:21 +0000

    Second commit, added link build functionality on load and foreach loop for streamer array to populate list

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
+const mainContainerEl = document.getElementById('main-container');
+const loadingEl = document.getElementById('loading');
 const twitchStreamContainerEl = document.getElementById('twitch-stream-container');
 const profilePictureEl = document.getElementById('profile-image');
 const streamerNameEl = document.getElementById('streamer-name');
+const streamerNameLinkEl = document.getElementById('streamer-name-url');
 const streamerOnlineOfflineEl = document.getElementById('online-offline');
 
-twitchArray = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+let isDuplicated = false;
+
+twitchArray = ["ESL_SC2", "OgamingSC2"];
+//  , "cretetion" , "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"
 testTwitchName = 'freecodecamp';
 
-twitchApiUrl = 'https://api.twitch.tv/helix/streams?query=';
-proxyApiUrl = 'https://twitch-proxy.freecodecamp.rocks/helix/streams?user_login=';
-proxyUrl = 'https://twitch-proxy.freecodecamp.rocks/twitch-api/users/freecodecamp';
-proxyServer='https://obscure-sands-82167.herokuapp.com/'
+proxyUrlUsers = `https://twitch-proxy.freecodecamp.rocks/twitch-api/users/`;
+proxyUrlStatus = `https://twitch-proxy.freecodecamp.rocks/twitch-api/streams/`;
+proxyServer='https://obscure-sands-82167.herokuapp.com/';
 
 // user_login - optional query param
 
 // need returned - user_name, game_name, type, 
 
-async function fetchTwitchAPI() {
+twitchArray.forEach(streamer => {
+    fetchTwitchAPI(streamer);  
+});
+
+async function fetchTwitchAPI(streamer) {
     try {
-            const res = await fetch(proxyServer + proxyUrl);
-            twitchRes = await res.json();
-            console.log(twitchRes);
+        const resUser = await fetch(proxyServer + proxyUrlUsers + streamer);
+        twitchUserRes = await resUser.json();
+        // const resStatus =  await fetch(proxyServer + proxyUrlStatus + streamer);
+        // twitchStatusRes = await resStatus.json();
+        // console.log(twitchStatusRes);
     } catch(error) {
         console.log(error);
     }
-    populateStreamers(twitchRes);
+   populateStreamers(twitchUserRes);
 }
 
-function populateStreamers(twitchRes) {
-    streamerNameEl.textContent = twitchRes.display_name;
-    profilePictureEl.src = twitchRes.logo;
-    streamerOnlineOfflineEl.textContent = twitchRes.type;
-    console.log(twitchRes._links.self);
-}
 
-fetchTwitchAPI();
+function populateStreamers(twitchUserRes) {
+    if (!isDuplicated) {
+        loadingEl.remove();
+        twitchStreamContainerEl.hidden = false;
+        // Populate streamer name and create link to twitch
+        streamerNameLinkEl.textContent = twitchUserRes.display_name;
+        streamerNameLinkEl.href = 'https://www.twitch.tv/' + twitchUserRes.display_name;
+        // Populate streamer profile picture
+        profilePictureEl.src = twitchUserRes.logo;
+        // Populate streamer type
+        streamerOnlineOfflineEl.textContent = twitchUserRes.type;
+        isDuplicated = true;
+    } else {
+        // Create new block
+        var newStreamerEl = twitchStreamContainerEl.cloneNode(true);
+        mainContainerEl.append(newStreamerEl);
+        newStreamerEl.hidden = false;
+        // Populate streamer name and create link to twitch
+        streamerNameLinkEl.textContent = twitchUserRes.display_name;
+        streamerNameLinkEl.href = 'https://www.twitch.tv/' + twitchUserRes.display_name;
+        // Populate streamer profile picture
+        profilePictureEl.src = twitchUserRes.logo;
+        // Populate streamer type
+        streamerOnlineOfflineEl.textContent = twitchUserRes.type;
+    }
+};
+
+// fetchTwitchAPI();
